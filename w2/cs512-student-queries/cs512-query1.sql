e3be545bde1e56da3e69edbf0a7d45c01d84b473
Aj Protzel <ajprotzel@gmail.com>
2025-01-20 14:01:13 -0800

    added files
    the two datasets used on MySQL and pdf instructions for setting up w1 sdk and w2 mysql

--- w2/cs512-student-queries/cs512-query1.sql
+++ w2/cs512-student-queries/cs512-query1.sql
@@ -1,2 +1,4 @@
 -- Select from films the title and rental duration of all films with an R
 -- rating and a rental cost of greater than $1 ordered by their title descending.
+
+SELECT title, rentalDuration from Films WHERE 
