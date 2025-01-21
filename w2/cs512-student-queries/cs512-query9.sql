38476331d4e7f07a832bd2e2d0a293d53cde3ad2
Aj Protzel <ajprotzel@gmail.com>
2025-01-20 17:14:49 -0800

    completes w2 assignment

--- w2/cs512-student-queries/cs512-query9.sql
+++ w2/cs512-student-queries/cs512-query9.sql
@@ -1,2 +1,30 @@
 -- Delete all rows from bsg_people where the person has no homeword
 
+DELETE FROM bsg_cert_people
+WHERE pid IN (
+    SELECT id 
+    FROM bsg_people 
+    WHERE homeworld IS NULL 
+        OR homeworld NOT IN (
+            SELECT id 
+            FROM bsg_planets
+            )
+    );
+
+DELETE FROM bsg_ship_assignment
+WHERE pid IN (
+    SELECT id 
+    FROM bsg_people 
+    WHERE homeworld IS NULL 
+        OR homeworld NOT IN (
+            SELECT id 
+            FROM bsg_planets
+            )
+    );
+
+DELETE FROM bsg_people
+WHERE homeworld IS NULL
+   OR homeworld NOT IN (
+    SELECT id 
+    FROM bsg_planets
+    );
