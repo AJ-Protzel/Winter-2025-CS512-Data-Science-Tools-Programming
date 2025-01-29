004b56664d98b6aee6899a0ac68214ba30114287
Aj Protzel <ajprotzel@gmail.com>
2025-01-28 17:25:27 -0800

    finished merger
    finished merger, added file descriptions, reorganized config file

--- w3/Transaction Pipline/main.py
+++ w3/Transaction Pipline/main.py
@@ -1,20 +1,19 @@
+# Adrien Protzel
+"""
+This program sequentially calls other scripts to import, manage, and clean data.
+"""
+
 import subprocess
 
 if __name__ == "__main__":
-    # Csk user to import files and to which folder <Type>_<Bank>_<Card>
-    subprocess.run(["python", "file_importer.py"])
-    print("File Importing......Done")
+    # # Csk user to import files and to which folder <Type>_<Bank>_<Card>
+    # subprocess.run(["python", "file_importer.py"])
+    # print("File Importing......Done")
 
-    # Cleans headers and merges multiple files in single bank account folder
+    # Cleans headers and merges multiple files in single bank account file
     subprocess.run(["python", "file_merger.py"])
     print("File Merging......Done")
 
-    # Removes, renames, adds, splits columns
-
-    # remove / fix headers
-    # merge like Files 
-
-    # remove unwanted cols
-    # rename cols
-    # add cols / split cols
-    # fix fields using maps
+    # Removes, renames, adds, splits columns, merges into single clean file in Clean folder
+    subprocess.run(["python", "file_cleaner.py"])
+    print("File Cleaning......Done")
